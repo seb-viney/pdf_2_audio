81bd6b68e31c1cd515cc21e590969eca4f60ad89
seb-viney <sebastian.r.viney@gmail.com>
2022-12-16 12:28:23 +1100

    first commit with initial code and update to gitignore

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-import pyttsx3,PyPDF2
+import os, pyttsx3, PyPDF2
 
+#File locations
+pdf_file = os.path.abspath("pdfs\TheGameDesignerPlaybook_EBook.pdf") #TODO be update to prompt the user to select a file
+output_mp3_file = os.path.abspath(f"outputs\{os.path.basename(pdf_file)}.mp3")
+
+pdf_reader = PyPDF2.PdfFileReader(open(pdf_file, 'rb'))
+
+audio = pyttsx3.init()
+
+for page_num in range(pdf_reader.numPages):
+    text = pdf_reader.getPage(page_num).extract_text()
+    clean_text = text.strip().replace('\n',' ')
+
+audio.save_to_file(clean_text, output_mp3_file)
+audio.runAndWait()
+
+audio.stop()
